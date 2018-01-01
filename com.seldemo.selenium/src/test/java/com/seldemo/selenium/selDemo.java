6b9d1092b22eaef62dc2b656a940990107f7211d
Sreedevi Pratap <Sreedevi Pratap@LAPTOP-O49DF7TP.home>
2018-01-01 13:47:23 +0100

    Commit 1

---
+++ com.seldemo.selenium/src/test/java/com/seldemo/selenium/selDemo.java
@@ -0,0 +1,12 @@
+package com.seldemo.selenium;
+import org.testng.annotations.Test;
+
+public class selDemo {
+	
+	@Test
+	public void PrintSel()
+	{
+		System.out.println("This is a selenium maven jenkins git demo");
+	}
+
+}
